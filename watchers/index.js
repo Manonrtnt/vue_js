247fe09d99f380707abee8641d6e3664eb1fff17
Manonrtnt <70939460+Manonrtnt@users.noreply.github.com>
2022-02-22 13:28:08 +0100

    add style dynamique

--- watchers/index.js
+++ watchers/index.js
@@ -4,6 +4,7 @@ Vue.createApp({
             nbr : 0,
             leNombre : 0,
             message : "Essaie encore !",
+            randNumber : Math.round(Math.random() *10),
         };
     },
     watch: {
@@ -13,15 +14,51 @@ Vue.createApp({
             }
         },
         leNombre(value){
-            if (value == 7){
+            if (value == this.randNumber){
                 this.message = " BINGO !";
-            } else if (value > 7){
+            } else if (value > this.randNumber){
                 this.message = "tu as dépassé le nombre mystère !"
                 this.leNombre = 0;
             }
             setTimeout(() => {
                 this.leNombre = 0;
+                // this.message = "Essaie encore !"
             }, 5000); 
         }
     },
 }).mount('#app');
+
+
+// CORRECTION :
+// Vue.createApp({
+//     data() {
+//         return {
+//             leNombre: 0,
+//             nombreMystere: Math.round(Math.random() * 10)
+//         };
+//     },
+//     computed: {
+//         indice() {
+//             if (this.leNombre < this.nombreMystere) {
+//                 return 'ESSAIE ENCORE!';
+//             } else if (this.leNombre === this.nombreMystere) {
+//                 return this.leNombre + '...BINGO';
+//             } else {
+//                 return 'tu as dépassé le nombre';
+//             }
+//         },
+//     },
+//     methods: {
+//         augmenter(num) {
+//             this.leNombre += num;
+//         },
+//     },
+//     watch: {
+//         leNombre(value) {
+//             console.log('le watcher en action...');
+//             setTimeout(() => {
+//                 this.leNombre = 0;
+//             }, 5000);
+//         },
+//     },
+// }).mount('#monApp');
